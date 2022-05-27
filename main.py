1fcd2890cccd0f05f81ac2d314eb258d7f3f02f2
Sharvin Suntoobacus <SharCoding@gmail.com>
2022-05-27 17:58:32 +0400

    before yield

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from pytube import Channel, YouTube
+"""Get information about a YouTube Channel using the YouTube API."""
+
+from typing import Any
+from pytube import Channel, YouTube  # type: ignore
+import pandas
+import datetime
+
+masterlist: list[dict[str, str]] = []
+
+c: Any = Channel('https://www.youtube.com/c/AdobeVideo/videos')
 
 
-c = Channel('https://www.youtube.com/channel/UCtJNlrOPYE25Th5ATbgpoow')
-for item in c:
-    yt = YouTube(item)
-    print(yt.author)
-    print(yt.description)
-    print(yt.length / 60)
-    print(yt.publish_date)
-    print(yt.title)
-    print(yt.views)
-    break
+def main(stopper: int):
+    """Run when the app starts."""
+    file_name: str | None = None
+    for index, item in enumerate(c):
+        if index > stopper:
+            break
+
+        masterdict: dict[str, Any] = {}
+
+        yt: Any = YouTube(item)
+        title: Any = yt.title
+
+        if not file_name:
+            file_name = yt.author.replace(' ', '_')
+
+        print(index, title)
+
+        masterdict['title'] = title
+        masterdict['minutes'] = str(datetime.timedelta(seconds=yt.length))
+        masterdict['date'] = yt.publish_date
+        masterdict['views'] = yt.views
+        masterdict['url'] = item
+        masterdict['description'] = yt.description
+
+        masterlist.append(masterdict)
+
+    df = pandas.DataFrame(masterlist)
+    df.to_csv(f'{file_name}_data.csv', ';', index=False)  # type: ignore
+
+
+if __name__ == "__main__":
+    main(366)
