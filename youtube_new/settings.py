431f551c7e534f553c02bf6bc725ed9cf0f0bf81
Sharvin Suntoobacus <SharCoding@gmail.com>
2022-06-16 07:33:06 +0400

    updates

--- youtube_new/settings.py
+++
@@ -1,6 +0,0 @@
-"""This file contains settings for the application."""
-
-# The following API keys are no longer valid. Replace with your own.
-
-# API_KEY = "AIzaSyBIiBoO2Z2U6AzbcBQV-6wfFKFoFfu_khQ"
-API_KEY = "AIzaSyAFyXou_mo5K0OWGluXgmKJRFZufZtLbhE"
