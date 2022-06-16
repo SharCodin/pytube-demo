35ced3829c89d2490e32714fcdf01a600f6ef3bf
SharCoding <66864721+SharCodin@users.noreply.github.com>
2022-06-16 06:56:14 +0400

    Update settings.py

--- youtube_new/settings.py
+++ youtube_new/settings.py
@@ -1,4 +1,6 @@
 """This file contains settings for the application."""
 
-# API_KEY = "AIzaSyBIiBoO2Z2U6AzbcBQV-6wfFKFoFfu_khQ"
+# The following API keys are no longer valid. Replace with your own.
+
+# API_KEY = "AIzaSyBIiBoO2Z2U6AzbcBQV-6wfFKFoFfu_khQ" 
 API_KEY = "AIzaSyAFyXou_mo5K0OWGluXgmKJRFZufZtLbhE"
